82eafa55b253c5de3c474deb548f14f07443d3d0
gjj-2k <84782991+gjj-2k@users.noreply.github.com>
2021-06-20 10:24:28 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,25 @@
+`CREATE TABLE publisher
+  (
+    id         integer    PRIMARY KEY   
+    name       text       NOT NULL
+    country    text       NOT NULL
+  );
+  
+  CREATE TABLE book
+  (
+    id           integer    PRIMARY KEY   
+    title        text       NOT NULL
+    publisher    text       references publisher(id)
+  );
+  
+  CREATE TABLE subjects
+  (
+    id         integer    PRIMARY KEY   
+    name       text       NOT NULL
+  );
 
+CREATE TABLE book_subjects
+  (
+    book          text      references books(id)    
+    subject       text      references subjects(id)
+  );
